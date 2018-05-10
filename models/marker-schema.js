d405964eb47ea90f01b19e2affe8eaa6989277c2
jaime <jaime.garvia@ikasle.aeg.es>
2018-05-10 16:34:41 +0200

    corregido float x string en schema

--- models/marker-schema.js
+++ models/marker-schema.js
@@ -4,8 +4,8 @@ const mongoose = require ('mongoose');
 const MarkerSchema = mongoose.Schema({
     
     url: {type : String , unique : true , "default" : '' , required : true},
-    latitud: {type: Float , unique : false , default : false , required : true},
-    longitud: {type: Float , unique : false , default : false , required : true},
+    latitud: {type: String , unique : false , default : false , required : true},
+    longitud: {type: String , unique : false , default : false , required : true},
     titulo: {type : String , unique : false , "default" : '' , required : true},
     comentario: {type : String , unique : false , "default" : '' , required : true}
         
